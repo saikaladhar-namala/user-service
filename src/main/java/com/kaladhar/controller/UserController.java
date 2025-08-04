fceef5fed2a5ba14b942473d91ad0be7c7d8413e
saikaladhar-namala <saikaladhar981@gmail.com>
2025-08-04 12:02:14 +0530

    added Java Spring boot Project

---
+++ src/main/java/com/kaladhar/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.kaladhar.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@GetMapping("/greet")
+	public ResponseEntity<String> greetMsg() {
+
+		return ResponseEntity.ok("Hi Welcome Kaladhar to AWS & Docker");
+
+	}
+}
