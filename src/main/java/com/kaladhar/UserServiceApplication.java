715131f89daa5fb6111f43f8b2b1a0af0b1c1832
Sai kaladhar <126257349+saikaladhar-namala@users.noreply.github.com>
2025-08-15 10:28:42 +0530

    Update UserServiceApplication.java

--- src/main/java/com/kaladhar/UserServiceApplication.java
+++ src/main/java/com/kaladhar/UserServiceApplication.java
@@ -2,13 +2,18 @@ package com.kaladhar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class UserServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserServiceApplication.class);
+    }
 }
